14164b8680abd4920c2277e465db434d25ff8cfd
scalalang2 <cailhuiris@gmail.com>
2025-12-28 20:27:01 +0900

    A boilerplate for Orleans Cluster

--- GameServer/GameLogicServer/Program.cs
+++ GameServer/GameLogicServer/Program.cs
@@ -1,3 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Orleans.Configuration;
+using Orleans.Providers;
 
-Console.WriteLine("Hello, World!");
+await Host.CreateDefaultBuilder(args)
+    .UseOrleans((context, siloBuilder) =>
+    {
+        var orleansConfig = context.Configuration.GetSection("Application");
+        siloBuilder.Configure<ClusterOptions>(orleansConfig);
+        
+        var clusteringConfig = orleansConfig.GetSection("Clustering");
+        var clusteringProvider = clusteringConfig.GetValue<string>("Provider"); 
+        switch (clusteringProvider)
+        {
+            case "Localhost":
+                siloBuilder.UseLocalhostClustering();
+                break;
+            case "Kubernetes":
+                siloBuilder.UseKubernetesHosting();
+                break;
+            default:
+                throw new Exception($"Unknown provider : {clusteringProvider}");
+        }
+
+        // Persistence
+        var persistenceConfig = orleansConfig.GetSection("Persistence");
+        var persistenceProvider = persistenceConfig.GetValue<string>("Provider");
+        switch (persistenceProvider)
+        {
+            case "DynamoDB":
+                siloBuilder.AddDynamoDBGrainStorage(ProviderConstants.DEFAULT_STORAGE_PROVIDER_NAME, options =>
+                {
+                    var ddbSection = persistenceConfig.GetSection("DynamoDB");
+                    options.TableName = ddbSection.GetValue<string>("TableName");
+                    options.Service = ddbSection.GetValue<string>("Service");
+                    options.ServiceId = ddbSection.GetValue<string>("ServiceId");
+                })
+                .UseTransactions();
+                break;
+            case "Memory":
+                siloBuilder.AddMemoryGrainStorage(ProviderConstants.DEFAULT_STORAGE_PROVIDER_NAME);
+                break;
+            default:
+                throw new Exception("Unknown persistence provider");
+        }
+    })
+    .ConfigureServices(services =>
+    {
+    })
+    .ConfigureLogging(logging =>
+    {
+        logging.AddConsole();
+    })
+    .RunConsoleAsync();
