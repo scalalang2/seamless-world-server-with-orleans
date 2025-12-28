fe83b5c4b3e0c17c8a569f11aba11e7f89495b44
scalalang2 <cailhuiris@gmail.com>
2025-12-28 22:29:20 +0900

    Add login & logout RPC

--- GameServer/GameGatewayServer/Services/GatewayServerImpl.cs
+++ GameServer/GameGatewayServer/Services/GatewayServerImpl.cs
@@ -5,6 +5,16 @@ namespace GameGatewayServer.Services;
 
 public class GatewayServerImpl : GatewayServer.GatewayServerBase
 {
+    public override Task<LoginResponse> Login(LoginRequest request, ServerCallContext context)
+    {
+        return base.Login(request, context);
+    }
+
+    public override Task<LogoutResponse> Logout(LogoutRequest request, ServerCallContext context)
+    {
+        return base.Logout(request, context);
+    }
+
     public override Task<PublishResponse> Publish(IAsyncStreamReader<PublishRequest> requestStream, ServerCallContext context)
     {
         return base.Publish(requestStream, context);
